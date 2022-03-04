b0fde99db49e90b8b6b56905f853161aca06f5e9
Danylo Hukov <danil.gukov@mail.ru>
2022-03-04 23:08:00 +0100

    Version 1.1.2
    -Fix esc menu
    - Hiding Mouse

--- Sanguis_Universum/Assets/Scripts/SceneMenager/PauseMenu.cs
+++ Sanguis_Universum/Assets/Scripts/SceneMenager/PauseMenu.cs
@@ -2,23 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MenuState { Play, Pause, }
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public MenuState menuState;
+    public void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            if(GameIsPaused && !pauseMenuUI.activeSelf)
-                Resume();
-            else
-                Pause();
-        }
+        if(Input.GetKeyDown(KeyCode.Escape) && menuState == MenuState.Play)
+            Pause();
+        else if (Input.GetKeyDown(KeyCode.Escape) && menuState == MenuState.Pause)
+            Resume();
     }
 
     public void Resume()
     {
+        menuState = MenuState.Play;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
@@ -26,6 +33,9 @@ public class PauseMenu : MonoBehaviour
     
     void Pause()
     {
+        menuState = MenuState.Pause;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
