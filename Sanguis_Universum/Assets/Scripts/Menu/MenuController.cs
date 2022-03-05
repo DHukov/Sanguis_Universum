f5a5910b53a5b5644eaca891a67932bfa409ded0
Danylo Hukov <danil.gukov@mail.ru>
2022-03-05 01:00:55 +0100

    1.1.3

--- Sanguis_Universum/Assets/Scripts/Menu/MenuController.cs
+++ Sanguis_Universum/Assets/Scripts/Menu/MenuController.cs
@@ -2,48 +2,28 @@
 using System.Collections;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
-using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
     public UnityEvent SaveUnit;
     public UnityEvent LoadUnit;
     public GameObject settings;
     public void NewGame()
     {
-
         StartCoroutine(NewScene());
-
-        //SaveUnit.Invoke();
     }
     IEnumerator NewScene()
     {
         AsyncOperation AsyncLoad = SceneManager.LoadSceneAsync(1);
-        //anim.SetTrigger("Fade");
         while (!AsyncLoad.isDone)
-        {
             yield return null;
-            //_player.GetComponent<PlayerStats>().LoadPlayerStats();
-
-        }
     }
-
-    public void LoadGame()
-    {
-        LoadUnit.Invoke();
-    }
-
-    public void Settings()
-    {
-        settings.SetActive(!settings.activeSelf);
-    }
-
-
-
-    public void ExitGame()
-    {
-        Application.Quit();
-    }
-
-    
+    public void LoadGame() { LoadUnit.Invoke(); }
+    public void Settings() { settings.SetActive(!settings.activeSelf); }
+    public void ExitGame() { Application.Quit(); }    
 }
